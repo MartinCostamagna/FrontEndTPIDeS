2b367bd5d9c32de2a34a42105699c5e1bc9ae4ba
MartinCostamagna <martinandrecosta@gmail.com>
2025-06-17 16:35:47 -0300

    Finalizacionde tabla de datos

--- src/app/pages/tabla-de-datos/tabla-de-datos.ts
+++ src/app/pages/tabla-de-datos/tabla-de-datos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tabla-de-datos',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './tabla-de-datos.html',
   styleUrl: './tabla-de-datos.css'
 })
