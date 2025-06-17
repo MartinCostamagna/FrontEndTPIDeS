e5ed6147e73296a48dc25ab44214bc5a72dd7230
MartinCostamagna <martinandrecosta@gmail.com>
2025-06-17 12:08:56 -0300

    Finalizado el registro y creacion de componentes faltantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { TemplateComponent } from './pages/template/template.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { Registro } from './pages/registro/registro';
+import { TablaDeDatos } from './pages/tabla-de-datos/tabla-de-datos';
+import { CrearPersona } from './pages/crear-persona/crear-persona';
+import { EditarPersona } from './pages/editar-persona/editar-persona';
 
 export const routes: Routes = [
   {
@@ -16,5 +19,8 @@ export const routes: Routes = [
     ],
   },
   { path: 'login', component: LoginComponent },
-  { path: 'registro', component: Registro }
+  { path: 'registro', component: Registro },
+  { path: 'TablaDeDatos', component: TablaDeDatos },
+  { path: 'CrearPersona', component: CrearPersona },
+  { path: 'EditarPersona', component: EditarPersona }
 ];
---
+++ src/app/pages/tabla-de-datos/tabla-de-datos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-de-datos',
+  imports: [],
+  templateUrl: './tabla-de-datos.html',
+  styleUrl: './tabla-de-datos.css'
+})
+export class TablaDeDatos {
+
+}
